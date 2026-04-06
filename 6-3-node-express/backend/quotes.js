39645ca06713bfebd81d4be63b53d374919b1107
Wajza <s202262140@kfupm.edu.sa>
2026-04-06 11:10:53 +0300

    TODO3

--- 6-3-node-express/backend/quotes.js
+++ 6-3-node-express/backend/quotes.js
@@ -1,5 +1,5 @@
 // Import random.js file
-
+import { getRandomInt } from "./utils/random.js";
 const quotes = [
   "Code is like humor. When you have to explain it, it’s bad.",
   "Before software can be reusable it first has to be usable.",
@@ -7,6 +7,10 @@ const quotes = [
   "Programming isn’t about what you know; it’s about what you can figure out."
 ];
 
-// TODO 3: Create and export getRandomQuote function
+// Done 3: Create and export getRandomQuote function
 //Hint: export function getRandomQuote(){}
+export function getRandomQuote() {
+  const index = getRandomInt(quotes.length);
+  return quotes[index];
+}
 
